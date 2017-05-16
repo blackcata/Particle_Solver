d45e33bce67151cf2593a9a53457cd166eac0235
blackcata <blackcata109@gmail.com>
2017-05-16 13:57:11 +0900

    Add Particle solver's 4 main codes

---
+++ Particle_main.f90
@@ -0,0 +1,13 @@
+!------------------------------------------------------------------------------!
+!                                                                              !
+!   PROGRAM : Particle_main.f90                                                !
+!                                                                              !
+!   PURPOSE : To simulate particles motions in the fluid                       !
+!                                                                              !
+!                                                             2017.05.16 K.Noh !
+!                                                                              !
+!------------------------------------------------------------------------------!
+
+        PROGRAM Particle_main
+
+        END PROGRAM Particle_main
