431307d18bd15fcc87c41eacb8434a347fa4bd30
blackcata <blackcata109@gmail.com>
2017-05-17 11:09:37 +0900

    Add Make file and add some modules in particle_module

---
+++ Makefile
@@ -0,0 +1,19 @@
+F90=ifort
+FCFLAGS=-O3 -qopenmp
+
+TARGET= Particle
+OBJECT= Particle_module.o Particle_main.o Particle_setup.o Particle_solver.o
+
+all : $(TARGET)
+$(TARGET) : $(OBJECT)
+	$(F90) $(FCFLAGS) -o $@ $^
+
+.SUFFIXES. : .o .f90
+
+%.o : %.f90
+	$(F90) $(FCFLAGS) -c $<
+
+clean :
+	rm -f *.o
+	rm -f *.mod
+	rm Particle 
--- Particle_module.f90
+++ Particle_module.f90
@@ -8,6 +8,33 @@
 !                                                                              !
 !------------------------------------------------------------------------------!
 
-        MODULE Particle_module
 
-        END MODULE
+        MODULE physical
+          IMPLICIT NONE
+          REAL(KIND=8) :: Lx, Ly, Lz, vol
+          REAL(KIND=8) :: time_accu
+          REAL(KIND=8) :: vper
+          REAL(KIND=8), PARAMETER :: PI=ACOS(-1.0)
+          REAL(KIND=8), PARAMETER :: g = 9.81
+        END MODULE physical
+
+        MODULE mesh
+          IMPLICIT NONE
+          INTEGER :: Nx, Ny, Nz, NxM, NyM, NzM
+          REAL(KIND=8) :: dx, dz, dx_sq, dz_sq
+          REAL(KIND=8), ALLOCATABLE, DIMENSION(:) :: y, dy, dym, dyc, dyp, dy_sq
+          REAL(KIND=8), ALLOCATABLE, DIMENSION(:) :: h, hm, hc, hp
+          REAL(KIND=8), ALLOCATABLE, DIMENSION(:) :: x1,x2,x3
+          REAL(KIND=8), PARAMETER :: gamma=1.9
+          INTEGER, ALLOCATABLE, DIMENSION(:) :: ipa, ima, kpa, kma,             &
+                                                jpa, jma, jmu, jmv
+        END MODULE mesh
+
+        MODULE particles
+          TYPE particle_type
+            INTEGER :: par_num                     ! Particles number
+            REAL(KIND=8) :: X_pos, Y_pos, Z_pos, & ! Particles position
+                            X_vel, Y_vel, Z_vel    ! Particles velocities
+          END TYPE particle_type
+
+        END MODULE particles
